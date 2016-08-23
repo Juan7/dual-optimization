295e8c54fd50cabf1976a5d87fc3efc837e2f3a2
Juan7 <juan.gomez.q@gmail.com>
2016-08-22 23:52:27 -0500

    need define choose best method

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from random import randint
 class Population:
     people = []
     def load_people(self):
-        self.people = [ [2,4], [4,5], [6,7], [14,2], [11,7]]
+        self.people = [[2,4,6,8], [4,5,8,12], [6,7,10,12], [14,2,45,33], [11,7,4,1]]
 
 class Fitness:
     functions = []
@@ -26,53 +26,60 @@ class Fitness:
             #print [fun(person[0],person[1]) for fun in self.functions]
         
 class Run:
-    mutation_factor = 0.7
+    
+    def __init__(self, mutation_factor=0.7, crossover_ratio=0.4, iterations=10)
+        self.mutation_factor = mutation_factor
+        self.combination_ratio = combination_ratio
+        self.iterations = iterations
+    
+    def get_new_index(people, taken_indexes):
+        first_index = randint(0,len(people))
+        while first_index in taken_indexes:
+            first_index = randint(0,len(people))
+        return first_index
     
     def get_indexes(people, current_index):
-        first_index = randint(0,len(people))
-        second_index = randint(0,len(people))
-        while current_index == first_index:
-            first_index = randint(0,len(people))
-        while current_index == second_index:
-            second_index = randint(0,len(people))
-        return first_index, second_index
+        taken_indexes = [current_index]
+        for i in range(0,3):
+            taken_indexes.append(self.get_new_index(people, taken_indexes))
+        return taken_indexes[1:]
     
     
-    def operate(people, current_index, first_index, second_index):
+    def operate(people, current_index, indexes):
         new_person = []
         for idx, value in enumerate(people[current_index]):
-            tmp_value = people[first_index][idx] - people[second_index][idx]
+            if random.uniform(0, 1) < self.crossover_ratio:
+                tmp_value = people[indexes[0]][idx] + self.mutation_factor * (people[indexes[1]][idx] - people[indexes[2]][idx])
+            else:
+                tmp_value = value
             new_person.append(tmp_value)    
         return new_person
     
     
-    def mutate(new_person):
-        for elem in new_person:
-            elem = elem * self.mutation_factor   
-        return new_person
-    
-    
-    def complete(person, new_person):
-        candidate_person = []
-        for idx, value in enumerate(new_person):
-            elem = person[idx] + new_person[idx]
-        return candidate_person
+#    def mutate(new_person):
+#        for elem in new_person:
+#            elem = elem * self.mutation_factor   
+#        return new_person
+#    
+#    
+#    def complete(person, new_person):
+#        candidate_person = []
+#        for idx, value in enumerate(new_person):
+#            elem = person[idx] + new_person[idx]
+#        return candidate_person
     
     
     def run(self):
         people = Population()
         people.load_people()
         
-        candidate = []
-        
-        for current_index, person in enumerate(people):
-            first_index, second_index = get_indexes(people, current_index)
-            new_person = operate(people, current_index, first_index, second_index)
-            new_person = mutate(new_person)
-            candidate_person = complete(person, new_person)
-            candidate.append(candidate_person)
-        
-        
+        for iteration in self.iterations:
+            next_generation = []
+
+            for current_index, person in enumerate(people):
+                indexes = get_indexes(people, current_index)
+                new_person = operate(people, current_index, indexes)
+                next_generation.append(choose_best(person, new_person))
         
         fit = Fitness()
         fit.start()
